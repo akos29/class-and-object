c6998d91bf992039fe01714e898c35f0dc1b1b8c
LAPTOP-4QVU197N\KB <kibrish49@gmail.com>
2023-02-06 13:51:37 +0200

    Add inheritance principle by creating dog and spider class

--- main.rb
+++ main.rb
@@ -1,4 +1,6 @@
 require "./animals.rb"
+require "./dog.rb"
+require "./spider.rb"
 
 p animal_1 = Animal.new("dog", 4, "Rex")
 p animal_1.id
@@ -13,3 +15,12 @@ p animal_2.name
 
 p animal_1.speak()
 p animal_2.speak()
+
+p dog = Dog.new("black", "Rex")
+p spider = Spider.new(85, "Wilma")
+
+p dog.bring_a_stick()
+# p spider.bring_a_stick()
+
+# p dog.make_a_web()
+p spider.make_a_web()
---
+++ spider.rb
@@ -0,0 +1,12 @@
+require "./animals.rb"
+
+class Spider < Animal
+  def initialize(web_strength_level, name = "Unknown")
+    super("spider", 8, name)
+    @web_strength_level = web_strength_level
+  end
+
+  def make_a_web
+    "www"
+  end
+end
