154ecd5b9387172f4a2980ae61e157b5e5f671d3
LAPTOP-4QVU197N\KB <kibrish49@gmail.com>
2023-02-06 13:21:29 +0200

    Add method to class animal

--- main.rb
+++ main.rb
@@ -1,13 +1,18 @@
 class Animal
-  def initialize
+  def initialize(number_of_legs, name = "Unknown")
     @id = Random.rand(1..1000)
-    @name = "Rex"
-    @number_of_legs = 4
+    @name = name
+    @number_of_legs = number_of_legs
+  end
+
+  def speak
+    "Bla bla bla"
   end
 end
 
-animal_1 = Animal.new
-animal_2 = Animal.new
+animal_1 = Animal.new(4, "Rex")
+animal_2 = Animal.new(8)
 
-p animal_1
-p animal_2
+
+p animal_1.speak
+p animal_2.speak
